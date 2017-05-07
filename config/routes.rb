c0ed83896c6527e24a984e12addca28fcf3bd9d1
epang1 <epang1@gmail.com>
2017-05-06 22:56:32 -0400

    done

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 RailsBlog::Application.routes.draw do
 
+  resources :comments, :only => [:create, :show] do 
+    collection do
+      post '/:id', to: 'comments#create'
+    end
+  end
+
   resources :users
   resources :tags
   resources :posts
